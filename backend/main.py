6b7c219a3caa8bfe56bac0f326e1c5c6c9308cc0
lanebo1 <k.efimovich@innopolis.university>
2024-06-05 11:22:38 +0300

    Added backend test code, refactored project structure

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
