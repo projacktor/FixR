a179afe56068c9396e5628ea26607915d19d0d22
lanebo1 <k.efimovich@innopolis.university>
2024-06-07 00:01:22 +0300

    Added requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import os
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
+# Mount the frontend directory at the root
+app.mount("/", StaticFiles(directory="../frontend", html=True), name="frontend")
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-
-@app.get("/main")
-async def main():
-    return FileResponse(os.path.join('..', 'frontend', 'index.html'), media_type='text/html')
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1:8000", port=8000)
