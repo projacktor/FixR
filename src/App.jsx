5f67ea88dab3a3c22589f29b728b308f7560bf3e
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-09 16:44:23 +0300

    Feature: Create Portfolio work1 page and Minibar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "./components/Header/Header.jsx";
 // import Home from "./pages/Home/Home.jsx";
 // import Footer from "./components/Footer/Footer.jsx";
-import Portfolio from "./pages/Portfolio/Portfolio.jsx";
+// import Portfolio from "./pages/Portfolio/Portfolio.jsx";
+import Work1 from "./pages/Portfolio work_1/Work1.jsx";
 
 function App() {
     return (
         <>
             <Header />
             {/*<Home />*/}
-            <Portfolio />
+            {/*<Portfolio />*/}
             {/*<Footer />*/}
+            <Work1 />
         </>
     )
 }
---
+++ src/pages/Portfolio work_1/Work1.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import "./style.scss";
+
+function Work1() {
+    return (
+        <main>
+            <div className="portfolio_project">
+                <div className="project_desc">
+                    <h1>Дом из кирпича 560 м2 на Барвихе </h1>
+                    <div className="description_bar">
+                        <div className="description_minibar">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Work1;
