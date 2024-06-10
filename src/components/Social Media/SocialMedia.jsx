ec494a7e9402ff734be21c4385d846e83625143b
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-10 13:55:42 +0300

    Fix: Social Media

--- src/components/Social Media/SocialMedia.jsx
+++ src/components/Social Media/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import "./styles.scss";
+import "./style.scss";
 import videoY from "../../pages/Home/static/works/videoY.png";
 import vkIco from "../../pages/Home/static/icons/vkIco.svg";
 import instagramIco from "../../pages/Home/static/icons/instagramIco.svg";
@@ -11,22 +11,24 @@ function SocialMedia() {
         <div className="social_media">
             <h1>Мы максимально <b>открыты</b> и <br/>
                 подтверждаем экспертность в социальных сетях </h1>
-            <img className="vid_temp" src={videoY}/>
-            <div className="social_media_enum">
-                <div className="social_media_single social_media1">
-                    <img src={vkIco}/>
-                    <h4>Вконтакте</h4>
-                    <p>1 500 подписчиков</p>
-                </div>
-                <div className="social_media_single social_media2">
-                    <img src={instagramIco}/>
-                    <h4>Instagram</h4>
-                    <p>15 500 подписчиков</p>
-                </div>
-                <div className="social_media_single social_media3">
-                    <img src={telegramIco}/>
-                    <h4>Telegram</h4>
-                    <p>500 подписчиков</p>
+            <div id="social_media_block">
+                <img className="vid_temp" src={videoY} alt="video"/>
+                <div className="social_media_enum">
+                    <a className="social_media_single social_media1">
+                        <img src={vkIco}/>
+                        <h4>Вконтакте</h4>
+                        <p>1 500 подписчиков</p>
+                    </a>
+                    <a className="social_media_single social_media2">
+                        <img src={instagramIco}/>
+                        <h4>Instagram</h4>
+                        <p>15 500 подписчиков</p>
+                    </a>
+                    <a className="social_media_single social_media3">
+                        <img src={telegramIco}/>
+                        <h4>Telegram</h4>
+                        <p>500 подписчиков</p>
+                    </a>
                 </div>
             </div>
         </div>
