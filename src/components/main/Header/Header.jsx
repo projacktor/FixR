90a11b781a22df89e2e7eea7200ec5d0b38cd3ed
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-17 20:31:39 +0300

    Fix Header style

--- src/components/main/Header/Header.jsx
+++ src/components/main/Header/Header.jsx
@@ -3,29 +3,27 @@ import React from 'react';
 import "./style.scss";
 
 import headerLogo from "../../../assets/svg/headerLogo.svg";
-import line from "../../../assets/svg/headerLine.svg";
 import clockIcon from "../../../assets/svg/headerClockIcon.svg";
 import phoneIcon from "../../../assets/svg/headerPhone.svg";
-import onlineCircle from "../../../assets/svg/onlineCircleHeader.svg"
+import onlineCircle from "../../../assets/svg/onlineCircleHeader.svg";
 import whatsapp from "../../../assets/svg/whatsappIcon.svg";
 import telegram from "../../../assets/svg/telegramIcon.svg";
 import profileIcon from "../../../assets/svg/profileIcon.svg";
 import menuBar from "../../../assets/svg/menuBar.svg";
-
 
 function Header() {
     return (
         <header>
             <div className="contacts">
                 <img className="contacts_header_logo" src={headerLogo}/>
-                <img className="contacts_header_line line1" src={line}/>
+                <div className="contacts_header_line line1"></div>
                 <p className="tagline"> Первый онлайн-сервис по ремонту и строительству <b>c фикс стоимостью</b></p>
-                <img className="contacts_header_line line2" src={line}/>
+                <div className="contacts_header_line line2"></div>
                 <div className="worktime_container">
                     <img className="clock" src={clockIcon}/>
                     <p className="worktime">Звоните Пн-Вс: 8:30 - 19:00</p>
                 </div>
-                <img className="contacts_header_line line3" src={line}/>
+                <div className="contacts_header_line line3"></div>
                 <div className="phone_container">
                     <img className="phone" src={phoneIcon}/>
                     <a className="phone_number"><p className="phone_number">+7 (966) 250-77-99</p></a>
