921928e8998a20866a1617f56db48c2fd8a358bb
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-18 22:16:40 +0300

    Fix CheckButton

--- src/components/page chunks/Presentation/Presentation.jsx
+++ src/components/page chunks/Presentation/Presentation.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 import "./style.scss"
 
 import groupPhoto from "../../../pages/Home/static/works/groupPhoto.png";
+import CheckButton from "../../helpers/CheckButton/CheckButton.jsx";
 
 function Presentation() {
     return (
         <div className="presentation">
             <h1>Над вашим объектом будет работать команда <b>настоящих фанатов</b> своего дела </h1>
             <img src={groupPhoto}/>
-            <button className="get_consulting check_button">
-                Оставить заявку на консультацию от специалиста
-            </button>
+            <CheckButton text="Оставить заявку на консультацию от специалиста"/>
         </div>
     );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -196,7 +196,7 @@ function Home() {
             </div>
 
             <div id="home_risk">
-                <Risk />
+                <Risk/>
             </div>
             <div id="home_works_gallery">
                 <div className="response_header">
@@ -215,52 +215,51 @@ function Home() {
                         </a>
                     </div>
                 </div>
-                <WorksGallery />
+                <WorksGallery/>
             </div>
 
             <div id="home_check_feedbacks">
                 <CheckFeedbacks/>
             </div>
-            {/*<button className="check_responses check_button">*/}
-            {/*    Смотреть все отзывы*/}
-            {/*</button>*/}
 
-            {/*<div className="planner">*/}
-            {/*    <div className="planner_textbox">*/}
-            {/*        <h1>Всего <b>4 шага и 1.5 часа</b> личного времени </h1>*/}
-            {/*        <p>отделяют вас от дома своей мечты</p>*/}
-            {/*    </div>*/}
-            {/*    <div className="planner_steps">*/}
-            {/*        <div className="planner_single_step planner_step1">*/}
-            {/*            <p className="planner_steps_t1">Выбор опции</p>*/}
-            {/*            <p className="planner_steps_t2">1 мин</p>*/}
-            {/*        </div>*/}
-            {/*        <div className="planner_single_step planner_step2">*/}
-            {/*            <p className="planner_steps_t1">Выбор подходящего тарифа</p>*/}
-            {/*            <p className="planner_steps_t2">30 мин</p>*/}
-            {/*        </div>*/}
-            {/*        <div className="planner_single_step planner_step3">*/}
-            {/*            <p className="planner_steps_t1">Удобный выбор*/}
-            {/*                планировки и материала</p>*/}
-            {/*            <p className="planner_steps_t2">60 мин</p>*/}
-            {/*        </div>*/}
-            {/*        <div className="planner_single_step planner_step4">*/}
-            {/*            <p className="planner_steps_t1">Проведение работ сервисом FIX-ремонт</p>*/}
-            {/*        </div>*/}
-            {/*        <div className="planner_single_step planner_step5">*/}
-            {/*            <p className="planner_steps_t1">Получите — распишитесь!:)</p>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*    <div className="planner_visual">*/}
-            {/*        <img src={plannerImg1}/>*/}
-            {/*        <p>Выбирайте подходящий тариф под ваш бюджет.*/}
-            {/*            Цена фиксированная!</p>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="planner">
+                <div id="planner_right">
+                    <div className="planner_textbox">
+                        <h2>Всего <b>4 шага и 1.5 часа</b> <br/> личного времени </h2>
+                        <p>отделяют вас от дома своей мечты</p>
+                    </div>
+                    <div className="planner_steps">
+                        <div className="planner_single_step" id="planner_step1">
+                            <p className="planner_steps_t1">Выбор опции</p>
+                            <p className="planner_steps_t2">1 мин</p>
+                        </div>
+                        <div className="planner_single_step" id="planner_step2">
+                            <p className="planner_steps_t1">Выбор подходящего тарифа</p>
+                            <p className="planner_steps_t2">30 мин</p>
+                        </div>
+                        <div className="planner_single_step" id="planner_step3">
+                            <p className="planner_steps_t1">Удобный выбор
+                                планировки и материала</p>
+                            <p className="planner_steps_t2">60 мин</p>
+                        </div>
+                        <div className="planner_single_step" id="planner_step4">
+                            <p className="planner_steps_t1">Проведение работ сервисом FIX-ремонт</p>
+                        </div>
+                        <div className="planner_single_step" id="planner_step5">
+                            <p className="planner_steps_t1">Получите — распишитесь!:)</p>
+                        </div>
+                    </div>
+                </div>
+                <div id="planner_left">
+                    <img src={plannerImg1} alt="FIX-remont_planner"/>
+                    <p>Выбирайте подходящий тариф под ваш бюджет.
+                        Цена фиксированная!</p>
+                </div>
+            </div>
 
-            {/*<div className="home_presentation">*/}
-            {/*    <Presentation />*/}
-            {/*</div>*/}
+            <div className="home_presentation">
+                <Presentation />
+            </div>
 
             {/*<div className="home_responsibility">*/}
             {/*    <Responsibility/>*/}
