b57280700650df829d9cf264b26d9e31ec513fc8
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-18 23:21:48 +0300

    Fix SeoDiv

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 import Header from "./components/main/Header/Header.jsx";
-// import Feedbacks from "./pages/Feedbacks/Feedbacks.jsx";
+import Feedbacks from "./pages/Feedbacks/Feedbacks.jsx";
 import Home from "./pages/Home/Home.jsx";
 // import Footer from "./components/Footer/Footer.jsx";
-// import Portfolio from "./pages/Portfolio/Portfolio.jsx";
-// import Work1 from "./pages/Portfolio work_1/Work1.jsx";
-// import Blog from "./pages/Blog/Blog.jsx";
-// import Contacts from "./pages/Contacts/Contacts.jsx";
+import Portfolio from "./pages/Portfolio/Portfolio.jsx";
+import Work1 from "./pages/Portfolio work_1/Work1.jsx";
+import Blog from "./pages/Blog/Blog.jsx";
+import Contacts from "./pages/Contacts/Contacts.jsx";
 
 function App() {
     return (
