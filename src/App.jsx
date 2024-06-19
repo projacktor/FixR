5f6e7947dfe1e851d8f8567fc5b49c98cf7f1166
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-19 12:53:40 +0300

    Add React-router-dom to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/main/Header/Header.jsx";
 import Home from "./pages/Home/Home.jsx";
 import Feedbacks from "./pages/Feedbacks/Feedbacks.jsx";
-// import Footer from "./components/Footer/Footer.jsx";
 import Portfolio from "./pages/Portfolio/Portfolio.jsx";
 import Work1 from "./pages/Portfolio work_1/Work1.jsx";
 import Blog from "./pages/Blog/Blog.jsx";
@@ -14,10 +13,19 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"" || "/"} element={<Home />}/>
+                    <Route path="/contacts" element={<Contacts />}/>
+                    <Route path="/portfolio" element={<Portfolio />}/>
+                    <Route path="/portfolio/work1" element={<Work1 />}/>
+                    <Route path="/blog" element={<Blog />}/>
+                    <Route path="/feedbacks" element={<Feedbacks/>}/>
+                </Routes>
+            </BrowserRouter>
+            {/*<Home />*/}
             {/*<Contacts/>*/}
             {/*<Portfolio />*/}
-            {/*<Footer />*/}
             {/*<Work1 />*/}
             {/*<Blog />*/}
             {/*<Feedbacks />*/}
