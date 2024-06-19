261243f26552905411cb43404679fe0538b2225d
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-19 15:16:56 +0300

    Fix WritesUs background

--- src/components/main/Footer/Footer.jsx
+++ src/components/main/Footer/Footer.jsx
@@ -5,12 +5,18 @@ import Symbol from "../../helpers/Symbol/Symbol.jsx";
 import styles from "./style.module.scss";
 
 import footerLogo from "../../../assets/svg/footerLogo.svg";
+import WriteUs from "../../helpers/Write us/WriteUs.jsx";
 
 function Footer() {
     return (
         <footer className={styles.footer}>
             <div id={styles.symbol_container}>
-                <Symbol header_src={footerLogo} lineColor="rgba(255, 255, 255, 0.5)"/>
+                <Symbol header_src={footerLogo} lineColor="rgba(255, 255, 255б 1.25)"/>
+            </div>
+            <p id={styles.copyright_text}>Copyright 2024. Все права защищены</p>
+            <a id={styles.privacy_footer}>Политика конфиденциальности</a>
+            <div id={styles.contacts_container}>
+                <WriteUs/>
             </div>
         </footer>
     );
