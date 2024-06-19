573b0d3baab2bbc3e76e7b5a399d3f1d8fdbd698
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-19 14:00:47 +0300

    Add contacts container component from header

---
+++ src/components/helpers/Contacts Container/ContactsContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import "./style.scss";
+import clockIcon from "../../../assets/svg/headerClockIcon.svg";
+import phoneIcon from "../../../assets/svg/headerPhone.svg";
+
+function ContactsContainer() {
+    return (
+        <div className="contacts_container">
+            <div className="worktime_container">
+                <img alt="clock_icon" className="clock" src={clockIcon}/>
+                <p className="worktime">Звоните Пн-Вс: 8:30 - 19:00</p>
+            </div>
+            <div className="contacts_header_line line3"></div>
+            <address className="phone_container">
+                <img alt="phone_icon" className="phone" src={phoneIcon}/>
+                <a target="_blank" className="phone_number" href="tel:+7 (966) 250-77-99"><p className="phone_number">+7
+                    (966) 250-77-99</p></a>
+            </address>
+        </div>
+    );
+}
+
+export default ContactsContainer;
--- src/components/main/Header/Header.jsx
+++ src/components/main/Header/Header.jsx
@@ -6,26 +6,21 @@ import WriteUs from "../../helpers/Write us/WriteUs.jsx";
 import "./style.scss";
 
 import headerLogo from "../../../assets/svg/headerLogo.svg";
-import clockIcon from "../../../assets/svg/headerClockIcon.svg";
-import phoneIcon from "../../../assets/svg/headerPhone.svg";
 import profileIcon from "../../../assets/svg/profileIcon.svg";
 import menuBar from "../../../assets/svg/menuBar.svg";
+import ContactsContainer from "../../helpers/Contacts Container/ContactsContainer.jsx";
 
 function Header() {
     return (
         <header>
-            <div className="contacts">
-                <Symbol header_src={headerLogo}/>
-                <div className="contacts_header_line line2"></div>
-                <div className="worktime_container">
-                    <img alt="clock_icon" className="clock" src={clockIcon}/>
-                    <p className="worktime">Звоните Пн-Вс: 8:30 - 19:00</p>
+            <div className="header_left">
+                <div id="symbol_part">
+                    <Symbol header_src={headerLogo}/>
                 </div>
-                <div className="contacts_header_line line3"></div>
-                <address className="phone_container">
-                    <img alt="phone_icon" className="phone" src={phoneIcon}/>
-                    <a target="_blank" className="phone_number" href="tel:+7 (966) 250-77-99"><p className="phone_number">+7 (966) 250-77-99</p></a>
-                </address>
+                <div className="contacts_header_line line2"></div>
+                <div id="contacts_part">
+                    <ContactsContainer/>
+                </div>
             </div>
 
             <WriteUs/>
