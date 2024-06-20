00d2a6176c78000d5a2a4c03cff22f5fbae50024
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-21 00:23:35 +0300

    Remove deploy changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <BrowserRouter basename="/FixR/">
+            <BrowserRouter>
                 <Routes>
                     <Route path="" element={<Home />}/>
                     <Route path="/contacts" element={<Contacts />}/>
