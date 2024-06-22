fdcf0eba45cac6e0ab57f310d6233fcf23068040
Arsen <106221641+projacktor@users.noreply.github.com>
2024-06-22 21:16:14 +0300

    Start Services page

--- src/components/helpers/Symbol/Symbol.jsx
+++ src/components/helpers/Symbol/Symbol.jsx
@@ -6,17 +6,13 @@ function Symbol({header_src, lineColor}) {
     const lineStyle = {
         borderLeft: `1px dashed ${lineColor}`,
     }
-
-    const bolder = {
-        color: '#000',
-    }
     return (
         <div className="symbol_block">
             <a href="/">
                 <img alt="FIX-remont_logo" className="contacts_header_logo" src={header_src}/>
             </a>
             <div className="contacts_header_line line1" style={lineStyle}></div>
-            <p className="tagline"> Первый онлайн-сервис по ремонту и строительству <b style={bolder}>c фикс стоимостью</b></p>
+            <p className="tagline"> Первый онлайн-сервис по ремонту и строительству <b>c фикс стоимостью</b></p>
         </div>
     );
 }
---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./style.scss";
+
+function Services() {
+    return (
+        <main className="services">
+
+        </main>
+    );
+}
+
+export default Services;
