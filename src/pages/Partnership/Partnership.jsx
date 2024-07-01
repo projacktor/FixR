d8854a0821f8f313ef501f3be4c2ed5e722ea510
Arsen <106221641+projacktor@users.noreply.github.com>
2024-07-01 22:59:48 +0300

    Finish Partnership page

--- src/pages/Partnership/Partnership.jsx
+++ src/pages/Partnership/Partnership.jsx
@@ -12,6 +12,19 @@ import plannerImage from "../../../public/assets/img/plannerImage2.png";
 import OrganizationGallery from "../../components/widgets/Organization gallery/OrganizationGallery.jsx";
 import Transparency from "../../components/widgets/Transparency/Transperency.jsx";
 import Risk from "../../components/widgets/Risk/Risk.jsx";
+import ReasonsList from "../../components/widgets/Reasons List/ReasonsList.jsx";
+import WorksGallery from "../../components/widgets/Works Gallery/WorksGallery.jsx";
+import responseYandex from "../../../public/assets/img/responseYandex.png";
+import responseOtzovik from "../../../public/assets/img/responseOtzovik.png";
+import CheckFeedbacks from "../../components/widgets/Check Feedbacks/CheckFeedbacks.jsx";
+import Presentation from "../../components/widgets/Presentation/Presentation.jsx";
+import Responsibility from "../../components/widgets/Responsibility/Responsibility.jsx";
+import SocialMedia from "../../components/widgets/Social Media/SocialMedia.jsx";
+import pseudoVid from "../Home/static/works/pseudoVid.png";
+import ToBlog from "../../components/widgets/To Blog/ToBlog.jsx";
+import FAQ from "../../components/widgets/FAQ/FAQ.jsx";
+import EngConsulting from "../../components/widgets/Eng Consulting/EngConsulting.jsx";
+import SeoDiv from "../../components/widgets/SEO part/seo_div.jsx";
 
 function Partnership() {
     return (
@@ -76,7 +89,49 @@ function Partnership() {
 
             <Risk/>
 
+            <ReasonsList
+                head_text={<h1>Избавим ваших клиентов <br/> <b>от типичных проблем</b> <br/> ремонта квартиры:</h1>}/>
 
+            <div id="home_works_gallery">
+                <div className="response_header">
+                    <div id="response_names">
+                        <p>Но лучше всего о нас расскажут</p>
+                        <h1>Выполненные работы <br/> и отзывы клиентов </h1>
+                    </div>
+                    <div id="response_bullets">
+                        <a className="response_bullet" target="_blank"
+                           href="https://yandex.ru/maps/org/fix_remont/110403249402/features/?ll=49.192026%2C55.826770&mode=search&sll=48.744609%2C55.752080&sspn=0.090294%2C0.027834&tab=features&text=%D1%84%D0%B8%D0%BA%D1%81%20%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82&z=12">
+                            <img alt="yandex_picture" src={responseYandex}/>
+                            <p><u>Смотреть отзывы на Яндекс</u></p>
+                        </a>
+                        <a className="response_bullet response_bullet2" target="_blank">
+                            <img alt="otzovik_picture" src={responseOtzovik}/>
+                            <p><u>Смотреть отзывы на Отзовик</u></p>
+                        </a>
+                    </div>
+                </div>
+                <WorksGallery/>
+            </div>
+
+            <CheckFeedbacks/>
+
+            <Presentation/>
+
+            <Responsibility/>
+
+            <SocialMedia/>
+
+            <div className="pseudo_vid">
+                <img alt="pseudo_video" src={pseudoVid}/>
+            </div>
+
+            <ToBlog/>
+
+            <FAQ/>
+
+            <EngConsulting/>
+
+            <SeoDiv/>
         </main>
     );
 }
